0525433b069d1f58729ef88e7d091cf88767caf7
minju5467 <71800775+minju5467@users.noreply.github.com>
2020-10-30 02:07:54 +0900

    pre setting

---
+++ src/test/java/com/choa/s4/SqlSessionTest.java
@@ -0,0 +1,19 @@
+package com.choa.s4;
+
+import static org.junit.Assert.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SqlSessionTest extends MyTestCase {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Test
+	public void test() {
+		assertNotNull(sqlSession);
+	}
+
+}
